fce78f28374e8fbd53287cd34ca38344a680d4a0
EndersonPro <enjaviga@gmail.com>
2020-10-09 22:56:19 -0500

    push to heroku2

---
+++ dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs = __importStar(require("fs"));
+var path = __importStar(require("path"));
+var apollo_server_1 = require("apollo-server");
+var resolvers_1 = __importDefault(require("./resolvers"));
+var typeDefs = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8');
+var server = new apollo_server_1.ApolloServer({ typeDefs: typeDefs, resolvers: resolvers_1.default });
+server.listen().then(function (_a) {
+    var url = _a.url;
+    console.log("Server running in " + url);
+});
+//# sourceMappingURL=index.js.map
