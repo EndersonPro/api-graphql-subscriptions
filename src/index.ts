433777dfc9c6002da4ba932ae3081c8982ea32c7
EndersonPro <enjaviga@gmail.com>
2020-10-11 14:24:05 -0500

    feat(refactor): refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,15 @@
-import * as  fs from 'fs';
-import * as path from 'path';
-import resolvers from './resolvers';
-// import express from 'express';
+import fs from 'fs';
+import path from 'path';
 import { ApolloServer } from 'apollo-server';
-// import cors from 'cors';
-// import { createServer } from 'http';
-// import { SubscriptionServer } from 'subscriptions-transport-ws'
-// import { execute, subscribe } from 'graphql';
-
-// const app = express();
-
-// // config cors middleware
-// app.use(cors());
+import resolvers from './resolvers';
 
 // config port
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
-// app.use('/graphql', express.json());
-
-// read to schema
 const typeDefs = fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf-8');
 
-const apolloServer  = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
+const apolloServer = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
 
 apolloServer.listen({ port: PORT }).then(({ url }) => {
     console.log(`Running in ${url}`)
 });
-
-// const server = createServer(app);
-
-// apolloServer.applyMiddleware({ app });
-
-// server.listen(PORT, () => {
-//     new SubscriptionServer({
-//         execute,
-//         subscribe,
-//         schema: typeDefs,
-//     }, {
-//         server,
-//         path: '/subscriptions'
-//     });
-//     console.log(`Server running in ${PORT}`);
-// });
